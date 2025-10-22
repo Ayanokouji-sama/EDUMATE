8be9463a7ff592e8a7f8889f0ddac24d30e73882
Mohammad Bilal <mohdbilal4291@gmail.com>
2025-10-22 22:43:25 +0530

    fixed ai tools

--- backend/content/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    # We'll add content endpoints later
-]
--- backend/models_distribution/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('generate/', views.generate_text, name='generate_text'),
-    path('check/', views.check_availability, name='check_availability'),
-]
